f128069eaac4593613efe690d9daa9c864b8e228
ABHISHEK PODDAR <abhishekpoddar6225@gmail.com>
2026-03-20 23:05:55 +0530

    Day20

--- src/March/Belarus.java
+++ src/March/Belarus.java
@@ -1,5 +1,5 @@
 package March;
-
+//Brute Force
 public class Belarus {
 //	Richest Customer Wealth
 //	Use prefix sum row-wise to compute sums efficiently across accounts.
@@ -10,18 +10,17 @@ public class Belarus {
 //	2nd customer has wealth = 10 
 //	3rd customer has wealth = 8
 //	The 2nd customer is the richest with a wealth of 10.
-	
 	public static void main(String[] args) {
 		int [] [] accounts = {{1,5},{7,3},{3,5}};
 		
 		int maxWealth=0;
 		
 		for(int i=0 ; i<accounts.length ; i++) {
-			int currentWealth=0;
-			for(int j=0 ; j<accounts[i].length ; j++) {
-				currentWealth+=accounts[i][j];
+			int prefix=accounts[i][0];
+			for(int j=1 ; j<accounts[i].length ; j++) {
+				prefix +=accounts[i][j];
 			}
-			maxWealth= Math.max(maxWealth, currentWealth);
+			maxWealth= Math.max(maxWealth, prefix);
 		}
 		
 		System.out.println(maxWealth);
