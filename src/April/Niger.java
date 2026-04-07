5374621a9e83c0743a10b4152ed174a613964d12
ABHISHEK PODDAR <abhishekpoddar6225@gmail.com>
2026-04-07 23:17:48 +0530

    April7

--- src/April/Niger.java
+++ src/April/Niger.java
@@ -1,22 +1,30 @@
 package April;
-import java.util.*;
 public class Niger {
-	//217. Contains Duplicate
-	//Input: nums = [1,2,3,1]
-	//Output: true
-	public static void main(String[] args) {
-		int [] nums = {1,2,3,4,1};
-		
-		int duplicate=0;
-		
-		Set<Integer> set = new HashSet<>();
-		
-		for(int i=0 ; i<nums.length ; i++) {
-			if(set.contains(nums[i])) {
-				duplicate=nums[i];
-			}
-			set.add(nums[i]);
+	// 217. Contains Duplicate
+	// Input: nums = [1,2,3,1]
+	// Output: true
+	//Slow fast pointer
+	private static int findDuplicate(int[] nums) {
+
+		int slow = nums[0];
+		int fast = nums[0];
+
+		do {
+			slow = nums[slow];// One moment
+			fast = nums[nums[fast]];// two moment
+		} while (slow != fast);
+
+		slow = nums[0];
+
+		while (slow != fast) {// Both one one moment
+			slow = nums[slow];
+			fast = nums[fast];
 		}
-		System.out.println(duplicate);
+		return slow;
+	}
+	public static void main(String[] args) {
+		int[] nums = { 1, 2, 3, 1 };
+
+		System.out.println(findDuplicate(nums));
 	}
 }
