357537261c080217ff8d42cff5c747a4c099dcd1
ABHISHEK PODDAR <abhishekpoddar6225@gmail.com>
2026-05-31 18:33:57 +0530

    31May

--- src/May/Congo.java
+++ src/May/Congo.java
@@ -7,35 +7,32 @@ public class Congo {
 
 	static Scanner congoInput = new Scanner(System.in);
 
-	private static boolean checkAnagram(String string1 , String string2) {
-		
-		string1 =string1.toLowerCase();
-		string2 =string2.toLowerCase();
-		
-		// First check lengths
-	     if (string1.length() != string2.length()) {
-	         return false;
-	     }	
-		
-	 int [] character = new int[26];
-	 
-	 
-	 for(int i=0 ; i<string1.length() ; i++) {
-		 
-		 character[string1.charAt(i)-'a']++;
-		 character[string2.charAt(i)-'a']--;
-	 }
-	 
-	 for(int i=0 ; i<character.length ; i++) {
-		 if(character[i]!=0) {
-			 return false;
-		 }
-	 }
-	 
-	return true;
+	private static boolean checkAnagram(String string1, String string2) {
+
+		string1 = string1.toLowerCase();
+		string2 = string2.toLowerCase();
+
+		if (string1.length() != string2.length())
+			return false;
+
+		int[] character = new int[26];
+
+		for (int i = 0; i < string1.length(); i++) {
+
+			character[string1.charAt(i) - 'a']++;
+			character[string2.charAt(i) - 'a']--;
+		}
+
+		for (int i = 0; i < character.length; i++) {
+			if (character[i] != 0)
+				return false;
+		}
+
+		return true;
 	}
 
 	public static void main(String[] args) {
+		
 		System.out.print("Enter the first String : ");
 		String string1 = congoInput.next();
 		System.out.println();
@@ -45,7 +42,5 @@ public class Congo {
 		System.out.println();
 
 		System.out.println(checkAnagram(string1, string2));
-
 	}
-
 }
