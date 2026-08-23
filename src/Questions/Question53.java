a7b27d0633064f7a170405db9764a27a16b2d051
ABHISHEK PODDAR <abhishekpoddar6225@gmail.com>
2026-08-23 09:59:18 +0530

    23aug

--- src/Questions/Question53.java
+++ src/Questions/Question53.java
@@ -3,16 +3,18 @@ package Questions;
 public class Question53 {
 	//Max Subarray Sum
 	
-	int [] nums = {3,-4,5,4,-1,7,-8};
-	int maxSum=Integer.MIN_VALUE;
-	int currentSum=0;
-	for(int i=0 ; i<nums.length ; i++) {
-		currentSum+=nums[i];
-		maxSum=Math.max(maxSum, currentSum);
-		if(currentSum<0) {
-			currentSum=0;
+	public static void main(String[] args) {
+		int [] nums = {3,-4,5,4,-1,7,-8};
+		int maxSum=Integer.MIN_VALUE;
+		int currentSum=0;
+		for(int i=0 ; i<nums.length ; i++) {
+			currentSum+=nums[i];
+			maxSum=Math.max(maxSum, currentSum);
+			if(currentSum<0) {
+				currentSum=0;
+			}
 		}
+		System.out.println(maxSum);
 	}
-	System.out.println(maxSum);
 
 }
