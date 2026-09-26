a2a1bfe3ef57de7e7b9feca329a3f4fdaae0b27e
ABHISHEK PODDAR <abhishekpoddar6225@gmail.com>
2026-09-26 12:07:50 +0530

    Sep26

--- src/Questions/Question704.java
+++ src/Questions/Question704.java
@@ -10,15 +10,15 @@ public class Question704 {
 	        while(start<=end){
 	            int mid = start+(end-start)/2;
 
-	            if(target>nums[mid]){
+	            if(target>nums[mid])
 	                start= mid+1;
-	            }
-	            else if(target<nums[mid]){
+	            
+	            else if(target<nums[mid])
 	                end=mid-1;
-	            }
-	            else{
+	            
+	            else
 	                return mid;
-	            }
+	            
 	        }
 	    return -1;    
 	    }
